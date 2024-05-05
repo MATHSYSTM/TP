7c34ae564420521368bb927244c0f7d65a9104bc
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-05 15:01:10 +0000

    Update courpresentation.js

--- TP3eme/TP1/courpresentation.js
+++ TP3eme/TP1/courpresentation.js
@@ -272,7 +272,7 @@ let navi=document.querySelector("#nv1");
 let suj=document.querySelector("#tab");
 if(larg>800){navi.style.left=""+Math.round((larg-800)/2)+"px";}
 if(larg<800){/*document.querySelector("#couvr").removeChild(svcvr);*/
- navi.style.left=""+30+"px";navi.style.right=""+30+"px";} 
+ /*navi.style.left=""+30+"px";navi.style.right=""+30+"px";*/} 
 let CLI=document.querySelector("#clic");let ftsz;
 if(larg>=800){CLI.style.fontSize="60px";ftsz=60;}
 if(larg<800){CLI.style.fontSize="90px";ftsz=90;} 
