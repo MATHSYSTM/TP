66d09e1d03523e62e172fd691b56ed9f3b08ef2b
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-05 04:06:43 +0000

    Add files via upload

---
+++ cod.js
@@ -0,0 +1,5 @@
+gatekey="maviec7lesmaths";
+
+document.querySelector("body").removeChild(document.querySelector("#change"));
+
+document.querySelector("body").removeChild(document.querySelector("#newcod"));
