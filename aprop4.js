9dc8a03426876514f480bc75fa8403b4d8312b39
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-10 11:56:49 +0000

    Delete aprop4.js

--- aprop4.js
+++
@@ -1,8 +0,0 @@
-
-if(photocor[4]==1){
-let imag=document.createElement("img");imag.setAttribute("id","photocorr4");
-imag.setAttribute("src","photocorr4.jpg");
-imag.addEventListener("click",()=>{playings();
-document.querySelector("#exer").removeChild(event.currentTarget);tbcorr[4]=0;});
-document.querySelector("#exer").append(imag);tbcorr[4]=1;
-}
