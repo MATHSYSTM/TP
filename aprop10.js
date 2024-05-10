00bd39e0ddf837f7669acecd6ed61feb4df952fd
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-10 11:54:20 +0000

    Delete aprop10.js

--- aprop10.js
+++
@@ -1,7 +0,0 @@
-if(photocor[10]==1){
-let imag=document.createElement("img");imag.setAttribute("id","photocorr10");
-imag.setAttribute("src","photocorr10.jpg");
-imag.addEventListener("click",()=>{playings();
-document.querySelector("#exer").removeChild(event.currentTarget);tbcorr[10]=0;});
-document.querySelector("#exer").append(imag);tbcorr[10]=1;
-}
