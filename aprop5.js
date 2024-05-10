b908d6a1f56087937caf825a852620cf18b5957a
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-10 11:57:15 +0000

    Delete aprop5.js

--- aprop5.js
+++
@@ -1,8 +0,0 @@
-
-if(photocor[5]==1){
-let imag=document.createElement("img");imag.setAttribute("id","photocorr5");
-imag.setAttribute("src","photocorr5.jpg");
-imag.addEventListener("click",()=>{playings();
-document.querySelector("#exer").removeChild(event.currentTarget);tbcorr[5]=0;});
-document.querySelector("#exer").append(imag);tbcorr[5]=1;
-}
