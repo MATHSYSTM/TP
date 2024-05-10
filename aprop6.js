40856f0c333b4c6a93f59977987c62e5f9e63f77
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-10 11:57:41 +0000

    Delete aprop6.js

--- aprop6.js
+++
@@ -1,9 +0,0 @@
-
-
-if(photocor[6]==1){
-let imag=document.createElement("img");imag.setAttribute("id","photocorr6");
-imag.setAttribute("src","photocorr6.jpg");
-imag.addEventListener("click",()=>{playings();
-document.querySelector("#exer").removeChild(event.currentTarget);tbcorr[6]=0;});
-document.querySelector("#exer").append(imag);tbcorr[6]=1;
-}
